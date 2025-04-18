aa8066bd472d2f41901ee51b98ad8968f7a9529f
rajiv mishra <999rajivmishra@gmail.com>
2025-04-18 21:24:29 +0530

    added clear fotter

--- src/components/Fotter.jsx
+++ src/components/Fotter.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 function Fotter() {
   return (
     <div className=' border-t-2 sm:gap-20 mx-auto max-w-4xl mt-5 py-2 px-7'>
-      <div className='py-5 text-center'>@2025 rajiv , all rights reserved</div>
+      <footer style="text-align: center; padding: 1rem 0;">
+  &copy; 2025 Rajiv Mishra. All rights reserved. |
+  <a href="https://github.com/tech-Rajiv" target="_blank" style="text-decoration: none;">
+    GitHub
+  </a>
+</footer>
     </div>
   )
 }
